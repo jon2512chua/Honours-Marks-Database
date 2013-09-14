5705dccccec95a0d54cc5a081b5232c69d661a28
Tim Lander <tim57282@hotmail.com>
2013-09-14 15:08:05 +0800

    My gui (hopefully it works this time)

---
+++ Honours-Marks-Database/src/gui/DisplayTools.java
@@ -0,0 +1,33 @@
+package gui;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class DisplayTools {
+	static CTabFolder display(Shell shell) {
+		final Composite displayComposite = new Composite(shell, SWT.BORDER);
+		displayComposite.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, true, 1, 1));
+		displayComposite.setLayout(new FillLayout());
+		
+		//Set up tabs
+		final CTabFolder toolsTabFolder = new CTabFolder(displayComposite, SWT.NONE);
+		toolsTabFolder.setSelectionBackground(Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT));
+		
+		//Create Backup Data
+		CTabItem tbtmTools = new CTabItem(toolsTabFolder, SWT.NONE);
+		tbtmTools.setText("Backup");
+		
+		Button btnNewButton = new Button(toolsTabFolder, SWT.NONE);
+		btnNewButton.setText("Backup Now!");
+		
+		tbtmTools.setControl(btnNewButton);
+
+		return toolsTabFolder;
+	}
+}

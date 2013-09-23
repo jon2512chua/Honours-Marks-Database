dda3d298dcf5af89754bf46df2c076f5d6320b92
Tim Lander <tim57282@hotmail.com>
2013-09-23 22:41:47 +0800

    Updated Javadoc.

--- Honours-Marks-Database-2/src/gui/CommonButtons.java
+++ Honours-Marks-Database-2/src/gui/CommonButtons.java
@@ -6,7 +6,19 @@ import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
+/**
+ * Class for dealing with common arrangements of buttons.</br>
+ * Designed to help give the UI a consistent feel.
+ * @author Tim Lander
+ */
 public class CommonButtons {
+	
+	/**
+	 * Creates two buttons , and places them in the bottom right corner.</br>
+	 * The buttons are 'Save Changes' and 'Discard Changes'
+	 * @param parent the composite to put the buttons in
+	 * @return the created buttons, in the order {Save Changes, Discard Changes}
+	 */
 	public static Button[] addSaveDiscardChangesButton(Composite parent) {
 		Composite buttonsComposite = new Composite(parent, SWT.NONE);
 		buttonsComposite.setLayout(new RowLayout(SWT.HORIZONTAL));

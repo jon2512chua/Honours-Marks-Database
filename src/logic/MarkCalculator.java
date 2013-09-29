c71254c647231b4e871780d83e6a5a71e1659b0d
Sam <sam.widenbar@gmail.com>
2013-09-29 14:00:17 +0800

    Added mark calculation

---
+++ src/logic/MarkCalculator.java
@@ -0,0 +1,243 @@
+import java.util.List;
+import java.lang.Math.*;
+
+/**
+ * Class that handles the automatic calculation of subassessment, assessment, and unit marks, from the
+ * supplied subassessment marks from individual markers.
+ */
+public class MarkCalculator
+{
+    /**
+     * Method to find the average of a subassessment's marks, including outliers.
+     * 
+     * @curSubAssess is the subassessment for which the average is being found
+     * @return the subassessment's average, including outliers
+     */
+    
+    public static double subAssessAverage(SubAssessment curSubAssess) {
+
+        int size = curSubAssess.getMarks().size();
+         
+        double subAssessAve = 0;
+        for (int i = 0; i < size; i++){
+
+            subAssessAve += curSubAssess.getMarks().get(i).getValue();
+  
+        }
+        
+        subAssessAve = subAssessAve/size;
+        return subAssessAve;
+    }
+    
+    /**
+     * Method to find the standard deviation of a subassessment's marks.
+     * 
+     * @curSubAssess is the subassessment for which the standard deviation is being found
+     * @return the subassessment's standard deviation
+     */
+    public static double standardDeviationCalc(SubAssessment curSubAssess) {
+        int size = curSubAssess.getMarks().size();
+        
+        double subAssessAve = subAssessAverage(curSubAssess);
+        
+        double standDev = 0;
+        for (int i = 0; i < size; i++){
+            
+            double diff = (curSubAssess.getMarks().get(i).getValue() - subAssessAve);
+            standDev += (diff*diff);
+        }
+        standDev = standDev / size;
+        standDev = Math.sqrt(standDev);
+        
+        return standDev;
+    }  
+  
+    /**
+     * Method to calculate which (if any) subassessment marks lie outside a range of two
+     * standard deviations from the average of the marks, and if it is outside the range, sets the 
+     * flag on that mark to indicate so.
+     * 
+     * @curSubAssess is the subassessment for which the outliers are being found and marked
+     */
+    public static void outlierMarks(SubAssessment curSubAssess){
+        
+        int size = curSubAssess.getMarks().size();
+        double average = subAssessAverage(curSubAssess);
+        double standDev = standardDeviationCalc(curSubAssess);
+
+        for (int i = 0; i < size; i++){
+  
+                if ( (curSubAssess.getMarks().get(i).getValue()) > (average + 2*standDev) ||
+                        (curSubAssess.getMarks().get(i).getValue()) < (average - 2*standDev) ) {
+                    curSubAssess.getMarks().get(i).setInsideRange(false);
+                } else {
+                    curSubAssess.getMarks().get(i).setInsideRange(true);
+                }
+
+        }
+        return;
+    }
+    
+    
+    /**
+     * Method to find the average of a subassessment's marks, excluding outliers, and set it as
+     * that subassessment's mark.
+     * 
+     * @curSubAssess is the subassessment for which the mark is being found
+     * @return the subassessment's average, excluding outliers, which is the final subassessment mark
+     */
+    public static double subAssessMarkCalc(SubAssessment curSubAssess){
+        
+        int size = curSubAssess.getMarks().size();
+         
+        double subAssessAve = 0;
+        int inRange = 0;
+        for (int i = 0; i < size; i++){
+            if (curSubAssess.getMarks().get(i).getInsideRange()){
+                subAssessAve += curSubAssess.getMarks().get(i).getValue();
+                inRange++;
+            }
+  
+        }
+        
+        subAssessAve = subAssessAve/inRange;
+        return subAssessAve;
+    }
+  
+    
+    
+    /**
+     * Method to calculate the mark for a student's assessment, based on all of the
+     * subassessment marks and their respective weightings.
+     * 
+     * @curSubAssess is the assessment for which the mark is being found
+     * @return the assessment's mark
+     */
+    public static double assessMarkCalc(Assessment curAssess){
+    
+        double assessmentMark = 0;
+        int subAssessNum = curAssess.getSubAssessments().size();
+        for (int i = 0; i<subAssessNum; i++){
+            assessmentMark += subAssessMarkCalc(curAssess.getSubAssessments().get(i));
+        }
+        
+        curAssess.setMark((int)assessmentMark);
+        
+        //not sure if we want this method to set the mark,
+        //or another method to call this one and set the mark
+        
+        return assessmentMark;
+    }
+
+    
+    /**
+     * Method to calculate the mark for an individual student's unit, based on all of the assessment
+     * marks and their respective weightings.
+     * 
+     * @curUnit is the unit that the mark is being found for
+     * @return the unit's mark
+     */
+    public static double unitMarkCalc(Unit curUnit){  
+        double unitMark = 0;
+        int assessNum = curUnit.getAssessments().size();
+        
+        for (int i = 0; i<assessNum; i++){
+            unitMark += ( ((double)curUnit.getAssessments().get(i).getMark())
+                            * (curUnit.getAssessments().get(i).getUnitPercent()) / 100);
+        }
+        
+        curUnit.setMark((int)unitMark);
+        
+        //not sure if we want this method to set the mark,
+        //or another method to call this one and set the mark
+        
+        return unitMark;
+        
+    }
+    
+    /**
+     * Method to iterate through a unit, calculating averages for sub assessments, then marks for assessments,
+     * then the mark for the unit itself. Excludes outlier marks from sub assessments.
+     * 
+     * @curUnit is the unit that the mark is being found for
+     * @return the unit's mark
+     */
+    public static double calculateWholeUnit(Unit curUnit){
+        int assessNum = curUnit.getAssessments().size();
+        
+        int[] subAssessNum = new int[assessNum];
+        for (int i = 0; i < assessNum; i++) {
+            subAssessNum[i]= curUnit.getAssessments().get(i).getSubAssessments().size();
+        }
+        
+        for (int i = 0; i < assessNum; i++) {
+            for (int j = 0; j < subAssessNum[i]; j++) {
+                outlierMarks( curUnit.getAssessments().get(i).getSubAssessments().get(j) );
+                
+            }
+            assessMarkCalc( curUnit.getAssessments().get(i) );
+        }
+
+        return unitMarkCalc(curUnit);
+    }
+    
+    /**
+     * Method to iterate through all units taken by a student, and set the mark for each of them,
+     * including all involved assessments and subassessments
+     * 
+     * @student is the student that is being iterated through
+     */
+    public static void calculateStudentMarks(Student curStudent){
+        int unitNum = curStudent.getDiscipline().size();
+        
+        for (int i = 0; i < unitNum; i++) {
+            calculateWholeUnit( curStudent.getDiscipline().get(i) );
+        }
+
+        return;
+    }
+    
+        
+    /**
+     * Method that calculates the grade for a unit, based on the mark.
+     * Currently ommitted due to no grade variable existing in the unit object in java, and I am not
+     * sure if we need this method.
+     * 
+     * @curUnit is the unit that the mark is being found for
+     * @return the unit's grade
+     *
+    public static void calculateUnitGrade(Unit curUnit){
+        int unitMark = curUnit.getMark();
+        switch (unitMark/10) {
+                
+            case 5:
+                //mark = 50-59
+                curUnit.setGrade("P");
+            case 6:
+                //mark = 60-69
+                curUnit.setGrade("CR");
+            case 7:
+                //mark = 70-79
+                curUnit.setGrade("D");
+            case 8:
+                //mark = 80-89
+                curUnit.setGrade("HD");
+            case 9:
+                //mark = 90-99
+                curUnit.setGrade("HD");
+            case 10:
+                //mark = 100
+                curUnit.setGrade("HD");
+            case default:
+                //mark = 0-49
+                if (unitMark > 44) {
+                    curUnit.setGrade("N+");
+                } else {
+                curUnit.setGrade("N");
+                }
+                
+        }
+        
+    }
+    **/
+}
---
+++ src/logic/testing.java
@@ -0,0 +1,150 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Class that handles the automatic calculation of subassessment, assessment, and unit marks, from the
+ * supplied subassessment marks from individual markers.
+ */
+public class testing
+{
+    /**
+     * Declaring objects for testing purposes
+     */
+    public Unit testUnit1;
+    public Unit testUnit2;
+    public Assessment testAssess1;
+    public Assessment testAssess2;
+    public Assessment testAssess3;
+    public SubAssessment testSub1;
+    public SubAssessment testSub2;
+    public SubAssessment testSub3;
+    
+    public Mark mark1;
+    public Mark mark2;
+    public Mark mark3;
+    public Mark mark4;
+    public Mark mark5;
+    public Mark mark6;
+    
+    public List<Mark> markSet1;
+    public List<Mark> markSet2;
+    public List<SubAssessment> subList1;
+    public List<SubAssessment> subList2;
+    public List<Assessment> assessList1;
+    public List<Assessment> assessList2;
+    public List<Unit> testDisc1;
+    
+    public Student testStud1;
+    
+    /**
+     * Initialising objects for testing purposes
+     */
+    public void initObjects(){
+        
+        testUnit1 = new Unit();
+        testUnit2 = new Unit();
+        testAssess1 = new Assessment();
+        testAssess2 = new Assessment();
+        testAssess3 = new Assessment();
+        testSub1 = new SubAssessment();
+        testSub2 = new SubAssessment();
+        testSub3 = new SubAssessment();
+        markSet1 = new ArrayList<Mark>();
+        markSet2 = new ArrayList<Mark>();
+        subList1 = new ArrayList<SubAssessment>();
+        subList2 = new ArrayList<SubAssessment>();
+        assessList1 = new ArrayList<Assessment>();
+        assessList2 = new ArrayList<Assessment>();
+        testStud1 = new Student();
+        testDisc1 = new ArrayList<Unit>();
+        
+        mark1 = new Mark();
+        mark2 = new Mark();
+        mark3 = new Mark();
+        mark4 = new Mark();
+        mark5 = new Mark();
+        mark6 = new Mark();
+        mark1.setValue(39);
+        mark2.setValue(40);
+        mark3.setValue(41);
+        mark4.setValue(50);
+        mark5.setValue(40);
+        mark6.setValue(41);
+        
+        markSet1.add(mark1);
+        markSet2.add(mark1);
+        markSet2.add(mark2);
+        markSet2.add(mark3);
+        markSet2.add(mark4);
+        markSet2.add(mark5);
+        markSet2.add(mark6);
+        
+        testSub1.setMarks(markSet1);
+        testSub2.setMarks(markSet2);
+        testSub3.setMarks(markSet1);
+        testSub1.setMaxMark(100);
+        testSub2.setMaxMark(50);
+        testSub3.setMaxMark(50);
+        
+        subList1.add(testSub1);
+        subList2.add(testSub2);
+        subList2.add(testSub3);
+        
+        testAssess1.setSubAssessments(subList1);
+        testAssess2.setSubAssessments(subList2);
+        testAssess3.setSubAssessments(subList1);
+        testAssess1.setUnitPercent(100);
+        testAssess2.setUnitPercent(50);
+        testAssess3.setUnitPercent(50);
+        
+        assessList1.add(testAssess1);
+        assessList2.add(testAssess2);
+        assessList2.add(testAssess3);
+        
+        testUnit1.setAssessments(assessList1);
+        testUnit2.setAssessments(assessList2);
+        
+        testDisc1.add(testUnit1);
+        testDisc1.add(testUnit2);
+        
+        testStud1.setDiscipline(testDisc1);
+    }
+    
+    /**
+        THIS METHOD WORKS ON ASSESSMENTS, NOW CHANGED, KEEPING THIS IN CASE
+        I WANT TO USE IT AS A REFERENCE, WHEN ITERATING THROUGH ASSESSMENTS I GUESS????
+    
+    public double[] subAssessAverage(int x) {
+        List<SubAssessment> curSubAssessmentsList;
+        if (x==1){        
+            curSubAssessmentsList = testAss1.getSubAssessments();
+        } else{
+            curSubAssessmentsList = testAss2.getSubAssessments();
+        }
+        
+        if (curSubAssessmentsList == null) {
+            return null;
+         }
+        int subAssessNum = curSubAssessmentsList.size();
+        
+        int[] size = new int[subAssessNum];
+        
+        for (int i = 0; i < subAssessNum; i++){
+             size[i] = curSubAssessmentsList.get(i).getMarks().size();
+        }
+        
+        double[] subAssessAves = new double[subAssessNum];
+        for (int i = 0; i < subAssessNum; i++){
+            for (int j = 0; j < size[i]; j++) {
+                subAssessAves[i] += curSubAssessmentsList.get(i).getMarks().get(j).getValue();
+            }
+            subAssessAves[i] = subAssessAves[i]/size[i];
+        }
+        
+        return subAssessAves;
+    }
+    **/
+    
+}

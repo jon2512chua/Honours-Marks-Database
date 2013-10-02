af5c9741b48979082e3b2ef8b545b6974593c82f
Tim Lander <tim57282@hotmail.com>
2013-10-02 17:03:59 +0800

    Made my data class non-final, so I can later test editing.

--- src/gui/Data.java
+++ src/gui/Data.java
@@ -4,24 +4,24 @@ package gui;
  * Test Data
  * @author Tim Lander
  */
-public final class Data {
-	public static final String StudentNumber[] = {		"20355999", "20199654", "10965484", "20698762", "10779849"};
-	public static final String StudentNameTitle[] = {	"Mr", 		"Mr", 		"Mr", 		"Ms", 		"Dr"};
-	public static final String StudentNameLast[] = {	"Smith", 	"Wright", 	"Jones", 	"Plane", 	"Main"};
-	public static final String StudentNameFirst[] = {	"John", 	"Henery", 	"Ejger", 	"Jane", 	"Mary"};
-	public static final String StudentDissTitle[] = {
+public class Data {
+	public static String StudentNumber[] = {		"20355999", "20199654", "10965484", "20698762", "10779849"};
+	public static String StudentNameTitle[] = {	"Mr", 		"Mr", 		"Mr", 		"Ms", 		"Dr"};
+	public static String StudentNameLast[] = {	"Smith", 	"Wright", 	"Jones", 	"Plane", 	"Main"};
+	public static String StudentNameFirst[] = {	"John", 	"Henery", 	"Ejger", 	"Jane", 	"Mary"};
+	public static String StudentDissTitle[] = {
 		"See the Person Within: \r\nVisual Culture \r\nand Alzheimer's \r\nDisease", //TODO: line wrapping (how?)
 		"School or Madrassa? PareLEFThoice and the Failure of State-Run Education in Pakistan", 
 		"English Language Development in Context: The Peer Social Networks and Interactions of Spanish-Speaking Adolescent Immigrant Students",
 		"The Role of Perceived Stigma, Stigmatization and Religiosity in Explaining Lifetime Use of Illicit Drugs in Emerging Adults",
 	"Branding the Nation: Mediating Space, Value, and Identity in the Context of Global Culture"};
-	public static final String StaffNumber[] = {	"90666325", "90554498", "90454598", "90321654", "90357849"};
-	public static final String StaffNameTitle[] = {"Mr",	"Mr", 		"Dr", 		"Miss", 	"Dr"};
-	public static final String StaffNameLast[] = {	"Jane",	"Smith", 	"Man", 		"Smith", 	"Stuwart"};
-	public static final String StaffNameFirst[] = {"Tom",	"Alex",		"Super", 	"Ellen", 	"Marther"};
-	public static final String Unit[] = {		"ENSC3001", 							"MCTX3420", 			"ANTH3000", 			"MECH4400", 								"CITS3200",					"OTHR1000"};
-	public static final String UnitName[] = {	"Mechanisims and Mulitbody Systems", 	"Mechatronics Design", 	"Anthropology Basics", 	"Engineering for Sustainable Development", 	"Professional Computing", 	"Some Other Unit"};
-	public static final String UnitPoints[] = {"6, 6, 6, 6"};
-	public static final String Assessment[] = {"Essay", "Report", "Oral Presenation", "Assignment"};
+	public static String StaffNumber[] = {	"90666325", "90554498", "90454598", "90321654", "90357849"};
+	public static String StaffNameTitle[] = {"Mr",	"Mr", 		"Dr", 		"Miss", 	"Dr"};
+	public static String StaffNameLast[] = {	"Jane",	"Smith", 	"Man", 		"Smith", 	"Stuwart"};
+	public static String StaffNameFirst[] = {"Tom",	"Alex",		"Super", 	"Ellen", 	"Marther"};
+	public static String Unit[] = {		"ENSC3001", 							"MCTX3420", 			"ANTH3000", 			"MECH4400", 								"CITS3200",					"OTHR1000"};
+	public static String UnitName[] = {	"Mechanisims and Mulitbody Systems", 	"Mechatronics Design", 	"Anthropology Basics", 	"Engineering for Sustainable Development", 	"Professional Computing", 	"Some Other Unit"};
+	public static String UnitPoints[] = {"6, 6, 6, 6"};
+	public static String Assessment[] = {"Essay", "Report", "Oral Presenation", "Assignment"};
 
 }

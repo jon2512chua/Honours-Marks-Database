411e90d4a27ac1f7f086e9a1bfa4bff52af651cc
Tim Lander <tim57282@hotmail.com>
2013-10-08 20:25:53 +0800

    Added code to print to a log file (currently commented out)

--- src/gui/MainLoader.java
+++ src/gui/MainLoader.java
@@ -1,5 +1,10 @@
 package gui;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import sessionControl.DerbyUtils;
 import sessionControl.Directories;
 
@@ -7,10 +12,21 @@ import sessionControl.Directories;
  * Main class. Acts as a preloader for MainUI.
  * @author Tim Lander
  */
+@SuppressWarnings("unused")	//TODO: remove
 public class MainLoader {
+	private static final String errorFile = "LogFile.log";
 
 	public static void main(String[] args) {
-		// TODO add filestream for error (Right at end) ADD a logfile
+		// TODO: undisable
+		//Prints Errors to a log file stored in the local directory
+		//TODO: maybe standardise errors, and timestamp errors
+		/*try {
+			FileOutputStream fos = new FileOutputStream(new File(errorFile));
+			System.setErr(new PrintStream(fos));
+		} catch (FileNotFoundException e) {
+			System.err.println("Warning: Unable to print to log file. Future errors/warning will print to stdout");
+			e.printStackTrace();
+		}*/
 		
 		//Load correct SWT.jar
 		SWTLoader.loadSwtJar();

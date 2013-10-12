364a8cce81acf6cbee669d52a172c09013f2d454
SamWidenbar <sam.widenbar@gmail.com>
2013-10-12 17:22:01 +0800

    Update Mark.java

--- src/orm/Mark.java
+++ src/orm/Mark.java
@@ -1,5 +1,7 @@
 package orm;
 
 public class Mark extends BaseMark {
-
+    public Mark(int subAssessmentID, int studentID, int markerID, SubAssessment subAssessment) {
+        super(subAssessmentID, studentID,  markerID, subAssessment);
+    }
 }

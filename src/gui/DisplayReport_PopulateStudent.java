1ac7b4e593c51e648ad703e98f950b4e50287184
Tim Lander <tim57282@hotmail.com>
2013-10-13 16:48:43 +0800

    Updated Student Report. refreshAll is now recursive, so any length of levels is now supported.

--- src/gui/DisplayReport_PopulateStudent.java
+++ src/gui/DisplayReport_PopulateStudent.java
@@ -57,42 +57,52 @@ public class DisplayReport_PopulateStudent {
 			for (Student s : allStudents) {
 				TreeItem student = new TreeItem(studentTree, SWT.NONE);
 				TreeItemMap.put(student, s.studentID);
+
 				TreeItem studentNameTitle = new TreeItem(student, SWT.NONE);
 				TreeItemMap.put(studentNameTitle, s.title);
 				studentNameTitle.setText(0, "Title");
+
 				TreeItem studentNameLast = new TreeItem(student, SWT.NONE);
-				TreeItemMap.put(studentNameTitle, s.lastName);
+				TreeItemMap.put(studentNameLast, s.lastName);
 				studentNameLast.setText(0, "Last Name");
+
 				TreeItem studentNameFirst = new TreeItem(student, SWT.NONE);
-				TreeItemMap.put(studentNameTitle, s.firstName);
+				TreeItemMap.put(studentNameFirst, s.firstName);
 				studentNameFirst.setText(0, "First Name");
+
 				TreeItem studentDissTitle = new TreeItem(student, SWT.NONE);
-				TreeItemMap.put(studentNameTitle, s.dissTitle);
+				TreeItemMap.put(studentDissTitle, s.dissTitle);
 				studentDissTitle.setText(0, "Dissertation Title");
 
-				/*for (Staff supervisor : s.supervisors) {
+				for (Staff supervisor : s.supervisors) {
 					TreeItem studentSuper = new TreeItem(student, SWT.NONE);
-					TreeItemMap.put(studentSuper, new StringBuffer ("TODO:stringbuffer staff"));
-				}*/
+					TreeItemMap.put(studentSuper, new StringBuffer (supervisor.getFullName()));	//TODO: fix for stringbuffer
+					studentSuper.setText(0, "Supervisor");
+				}
 
-				/*List<Unit> units = s.getDiscipline();
+				List<Unit> units = s.getDiscipline();
 				for (Unit u : units) {
 					TreeItem unit = new TreeItem(student, SWT.NONE);
-					unit.setText(new String[] {u.getUnitCode()});
-					TreeItem unitName = new TreeItem(unit, SWT.NONE);
-					unitName.setText(new String[] {"Unit Name", u.getName()});
-					TreeItem unitPoints = new TreeItem(unit, SWT.NONE);
-					unitPoints.setText(new String[] {"Unit Points", u.getPoints()+""});
-					TreeItem unitMarks = new TreeItem(unit, SWT.NONE);
-					unitMarks.setText(new String[] {"Unit Mark", u.getMark()+""});
+					TreeItemMap.put(unit, u.unitCode);
 
+					TreeItem unitName = new TreeItem(unit, SWT.NONE);
+					TreeItemMap.put(unitName, u.name);
+					unitName.setText(0, "Unit Name");
+
+					TreeItem unitPoints = new TreeItem(unit, SWT.NONE);
+					TreeItemMap.put(unitPoints, u.points);
+					unitPoints.setText(0, "Unit Points");
+
+					TreeItem unitMarks = new TreeItem(unit, SWT.NONE);
+					TreeItemMap.put(unitMarks, u.mark);
+					unitMarks.setText(0, "Unit Mark");
 
 					List<Assessment> assessments = u.getAssessments();
 					for (Assessment a : assessments) {
 						TreeItem assessment = new TreeItem(unit, SWT.NONE);
-						assessment.setText(new String[] {a.getAssessmentID()});
+						TreeItemMap.put(assessment, a.name);
 					}
-				}*/
+				}
 
 			}
 
@@ -101,49 +111,6 @@ public class DisplayReport_PopulateStudent {
 			System.out.println("Whoops, null pointer exception");
 			e.printStackTrace();
 		}
-
-		/*for (int sn=0; sn<5; sn++) {
-			TreeItem student = new TreeItem(studentTree, SWT.NONE);
-			student.setText(new String[] {Data.StudentNumber[sn]});
-			TreeItem studentNameTitle = new TreeItem(student, SWT.NONE);		//TODO: in theory these could be combined into one row
-			studentNameTitle.setText(new String[] {"Title", Data.StudentNameTitle[sn]});
-			TreeItem studentNameLast = new TreeItem(student, SWT.NONE);
-			studentNameLast.setText(new String[] {"Last Name", Data.StudentNameLast[sn]});
-			TreeItem studentNameFirst = new TreeItem(student, SWT.NONE);
-			studentNameFirst.setText(new String[] {"First Name", Data.StudentNameFirst[sn]});
-			TreeItem studentDissTitle = new TreeItem(student, SWT.NONE);
-			studentDissTitle.setText(new String[] {"Dissertation Title", Data.StudentDissTitle[sn]});
-			TreeItem studentSuper = new TreeItem(student, SWT.NONE);
-			studentSuper.setText(new String[] { "Supervisor", Data.StaffNameTitle[sn] + " " + Data.StaffNameFirst[sn].charAt(0) + ". " + Data.StaffNameLast[sn]});
-
-			for (int units=sn/2; units<=sn/2+3; units++) {
-				TreeItem unit = new TreeItem(student, SWT.NONE);
-				unit.setText(new String[] {Data.Unit[units]});
-				TreeItem unitName = new TreeItem(unit, SWT.NONE);
-				unitName.setText(new String[] {"Unit Name", Data.UnitName[units]});
-				TreeItem studentUnitMark = new TreeItem(unit, SWT.NONE);
-				studentUnitMark.setText(new String[] {"Students Unit Mark", "100%"});
-				TreeItem studentUnitGrade = new TreeItem(unit, SWT.NONE);
-				studentUnitGrade.setText(new String[] {"Students Unit Grade", "HD"});
-				TreeItem unitPoints = new TreeItem(unit, SWT.NONE);
-				unitPoints.setText(new String[] {"Unit Points", "6"});
-
-				for (int assessments=0; assessments<4; assessments++) {
-					TreeItem assessment = new TreeItem(unit, SWT.NONE);
-					assessment.setText(new String[] {Data.Assessment[assessments]});
-					TreeItem studentAssessmentMark = new TreeItem(assessment, SWT.NONE);
-					studentAssessmentMark.setText(new String[] {"Student Assessment Mark", (10*Data.marks[sn*5+units*4+assessments])+"%"});
-					TreeItem AssessmentPercentUnit = new TreeItem(assessment, SWT.NONE);
-					AssessmentPercentUnit.setText(new String[] {"Percent of Unit", "22%"});
-					TreeItem AssessmentPercentFinalGrade = new TreeItem(assessment, SWT.NONE);
-					AssessmentPercentFinalGrade.setText(new String[] {"Percent of Final Grade", "5%"});
-					//TODO: add in markers. May be a bit tricky, as the number of markers are variable.
-				}
-
-			}
-
-			//student.setExpanded(true);		//TODO:Perhaps add an expand all button?
-		}*/
 
 		refreshAll(studentTree);
 
@@ -176,14 +143,42 @@ public class DisplayReport_PopulateStudent {
 	public static void refreshAll(Tree tree) {
 		for ( TreeItem ti : tree.getItems() ) {
 			ti.setText(TreeItemMap.get(ti).toString());
+			
+			refreshLevel(ti);
 
-			for ( TreeItem ti2 : ti.getItems() ) {
+			/*for ( TreeItem ti2 : ti.getItems() ) {
 				try {
-					ti.setText(1, TreeItemMap.get(ti2).toString());
+					if (ti2.getText(0).length() > 2)
+						ti2.setText(1, TreeItemMap.get(ti2).toString());
+					else ti2.setText(0, TreeItemMap.get(ti2).toString());
 				} catch (java.lang.NullPointerException e) {
-					ti.setText(1, "");
+					ti2.setText(1, "");
 				}
+				for ( TreeItem ti3 : ti2.getItems() ) {
+					try {
+						if (ti3.getText(0).length() > 2)
+							ti3.setText(1, TreeItemMap.get(ti3).toString());
+						else ti3.setText(0, TreeItemMap.get(ti3).toString());
+					} catch (java.lang.NullPointerException e) {
+						ti3.setText(1, "");
+					}
+				}
+			}*/
+		}
+	}
+	
+	private static void refreshLevel(TreeItem parent) {
+		for ( TreeItem ti : parent.getItems() ) {
+			try {
+				if (ti.getText(0).length() > 2)
+					ti.setText(1, TreeItemMap.get(ti).toString());
+				else ti.setText(0, TreeItemMap.get(ti).toString());
+			} catch (java.lang.NullPointerException e) {
+				ti.setText(1, "");
 			}
+			
+			if (ti.getItemCount() > 0)
+				refreshLevel (ti);
 		}
 	}
 

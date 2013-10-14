cae61ba85a5b19a42ab844ac4e2c3e7deca02eb7
Jonathan Chua <me@jonathanchua.com>
2013-10-15 00:29:48 +0800

    Formatting change.

--- src/orm/Unit.java
+++ src/orm/Unit.java
@@ -3,12 +3,11 @@ package orm;
 import java.sql.SQLException;
 
 public class Unit extends BaseUnit {
-	
-	public Unit(String unitCode) {
-		super(unitCode);
-	}
-	
-	public Unit(String unitCode, int studentID) throws SQLException {
-		super(unitCode, studentID);
-	}
+    public Unit(String unitCode) {
+            super(unitCode);
+    }
+
+    public Unit(String unitCode, int studentID) throws SQLException {
+            super(unitCode, studentID);
+    }
 }

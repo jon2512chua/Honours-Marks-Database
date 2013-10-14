30e677474a1928c0aab4888bacda0820571b29d9
Jonathan Chua <me@jonathanchua.com>
2013-10-15 00:24:25 +0800

    Forgot to specify that the constructor needs to decalre exception throwing as well.

--- src/orm/Mark.java
+++ src/orm/Mark.java
@@ -1,11 +1,13 @@
 package orm;
 
+import java.sql.SQLException;
+
 public class Mark extends BaseMark {
     public Mark(int subAssessmentID, int studentID, int markerID, SubAssessment subAssessment) {
         super(subAssessmentID, studentID,  markerID, subAssessment);
     }
     
-    public Mark(double value, String report, boolean insideRange, int markerID, int studentID, SubAssessment parentSubAssessment) {
+    public Mark(double value, String report, boolean insideRange, int markerID, int studentID, SubAssessment parentSubAssessment) throws SQLException {
         super(value, report, insideRange, markerID, studentID, parentSubAssessment);
     }
 }

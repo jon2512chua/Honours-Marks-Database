e696021518ad58951e1e9a07626ea1c38c10dac2
Nicholas Abbey <20522805@student.uwa.edu.au>
2013-10-15 22:57:56 +0800

    Added password change, added sec qn/ans to System schema, added zip of new System DB (need to install you guys), changed login methods, made shell script for running in OSX

--- src/gui/DisplayCE_PopulateEditStudent.java
+++ src/gui/DisplayCE_PopulateEditStudent.java
@@ -230,7 +230,6 @@ public class DisplayCE_PopulateEditStudent {
 			lastName.setText(student.getLastName()+"");
 			firstName.setText(student.getFirstName()+"");
 			dissertationTitle.setText(student.getDissTitle()+"");
-			//TODO: supervisor/s NLA!
 			for (TreeItem ti : supervisorTree.getItems()) {
 				ti.setChecked(false);
 				List<Staff> supervisors = student.getSupervisors();

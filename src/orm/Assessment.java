c9e93781b038702c49b98a6a15f3f63a7dc905a4
Jonathan Chua <me@jonathanchua.com>
2013-10-15 10:29:28 +0800

    Formatting changes.

--- src/orm/Assessment.java
+++ src/orm/Assessment.java
@@ -2,9 +2,9 @@ package orm;
 
 public class Assessment extends BaseAssessment {
 
-	public Assessment(int assessmentID) {
-		super(assessmentID);
-	}
+    public Assessment(int assessmentID) {
+        super(assessmentID);
+    }
 	
     public Assessment(int assessmentID, int studentID, Unit unit) {
     	super(assessmentID, studentID,  unit);

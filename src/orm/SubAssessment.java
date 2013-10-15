d8d01e034c38a69dbd012679e5abf1e4c96ee1a8
Jonathan Chua <me@jonathanchua.com>
2013-10-15 10:25:10 +0800

    Formatting change.

--- src/orm/SubAssessment.java
+++ src/orm/SubAssessment.java
@@ -1,11 +1,11 @@
 package orm;
 
 public class SubAssessment extends BaseSubAssessment {
-	public SubAssessment(int subAssessmentID) {
-		super(subAssessmentID);
-	}
-	
-	public SubAssessment(int subAssessmentID, int studentID, Assessment assessment) {
-		super(subAssessmentID, studentID, assessment);
-	}
+    public SubAssessment(int subAssessmentID) {
+        super(subAssessmentID);
+    }
+
+    public SubAssessment(int subAssessmentID, int studentID, Assessment assessment) {
+        super(subAssessmentID, studentID, assessment);
+    }
 }

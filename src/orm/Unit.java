7b8106a5ebc02d0e0e2450a8250d47c7b4231405
Jonathan Chua <me@jonathanchua.com>
2013-10-15 10:24:23 +0800

    Can get all units now.

--- src/orm/Unit.java
+++ src/orm/Unit.java
@@ -1,6 +1,9 @@
 package orm;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.*;
+import java.util.logging.*;
+import sessionControl.Session;
 
 public class Unit extends BaseUnit {
     public Unit(String unitCode) {
@@ -13,5 +16,22 @@ public class Unit extends BaseUnit {
     
     public Unit(String unitCode, String unitName, int unitPoints) throws SQLException {
         super(unitCode, unitName, unitPoints);
+    }
+    
+    public static List<Unit> getAllUnits() {
+        List<Unit> allUnits = new ArrayList<>();
+        
+        try {
+            Statement s = Session.dbConn.getConnection().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet unitsRS = s.executeQuery("SELECT UnitCode FROM Student");
+            
+            while (unitsRS.next()) {
+                allUnits.add(new Unit(unitsRS.getString("UnitCode")));
+            }
+        } catch (java.lang.NullPointerException | SQLException ex) {
+            Logger.getLogger(BaseStudent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return allUnits;
     }
 }

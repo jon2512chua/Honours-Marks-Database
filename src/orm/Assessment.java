f0d7f4ed8a175550f0a69b595112364306681e98
Jonathan Chua <me@jonathanchua.com>
2013-10-15 10:31:13 +0800

    Can now get all assessments.

--- src/orm/Assessment.java
+++ src/orm/Assessment.java
@@ -1,5 +1,10 @@
 package orm;
 
+import java.sql.*;
+import java.util.*;
+import java.util.logging.*;
+import sessionControl.Session;
+
 public class Assessment extends BaseAssessment {
 
     public Assessment(int assessmentID) {
@@ -8,5 +13,22 @@ public class Assessment extends BaseAssessment {
 	
     public Assessment(int assessmentID, int studentID, Unit unit) {
     	super(assessmentID, studentID,  unit);
+    }
+    
+    public static List<Assessment> getAllAssessments() {
+        List<Assessment> allAssessments = new ArrayList<>();
+        
+        try {
+            Statement s = Session.dbConn.getConnection().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet assessmentRS = s.executeQuery("SELECT UnitCode FROM Student");
+            
+            while (assessmentRS.next()) {
+                allAssessments.add(new Assessment(assessmentRS.getInt("UnitCode")));
+            }
+        } catch (java.lang.NullPointerException | SQLException ex) {
+            Logger.getLogger(BaseStudent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return allAssessments;
     }
 }

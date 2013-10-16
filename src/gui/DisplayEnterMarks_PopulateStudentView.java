3b3413f4acea0eacaa232ce072237a45b59b2872
Tim Lander <tim57282@hotmail.com>
2013-10-16 17:24:55 +0800

    Can enter marks, but changes are not updated.

--- src/gui/DisplayEnterMarks_PopulateStudentView.java
+++ src/gui/DisplayEnterMarks_PopulateStudentView.java
@@ -9,6 +9,12 @@ import logic.CohortData;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.custom.ControlEditor;
+import org.eclipse.swt.custom.TreeCursor;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridLayout;
@@ -17,6 +23,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.swt.widgets.TreeItem;
@@ -124,6 +131,81 @@ public class DisplayEnterMarks_PopulateStudentView {
 			}
 		});
 
+		//set up editing
+		final TreeCursor cursor = new TreeCursor(marksTree, SWT.NONE);	//TODO: fix border
+		cursor.setForeground(cursor.getDisplay().getSystemColor(SWT.COLOR_BLACK));
+
+		final ControlEditor editor = new ControlEditor(cursor);
+		editor.grabHorizontal = true;
+		editor.grabVertical = true;
+
+		//set up editing listeners
+		cursor.addSelectionListener(new SelectionAdapter() {
+
+			// when the TreeEditor is over a cell, select the corresponding row in the tree
+			public void widgetSelected(SelectionEvent e) {
+				if (cursor.getColumn() > 4 && cursor.getRow().getText(cursor.getColumn()) != "") {
+					cursor.setBackground(cursor.getDisplay().getSystemColor(SWT.COLOR_TITLE_BACKGROUND_GRADIENT));
+					marksTree.setSelection(new TreeItem[] { cursor.getRow() });
+				} else {
+					cursor.setBackground(cursor.getRow().getBackground(cursor.getColumn()));
+					cursor.setForeground(cursor.getRow().getForeground(cursor.getColumn()));
+				}
+			}
+
+			// when the user hits "ENTER" in the TreeCursor, pop up a text
+			// editor so that they can change the text of the cell
+			public void widgetDefaultSelected(SelectionEvent e) {
+				if (cursor.getColumn() > 4 && cursor.getRow().getText(cursor.getColumn()) != "") {
+					final Text text = new Text(cursor, SWT.NONE);
+					Validation.validateDouble(text);
+
+					TreeItem row = cursor.getRow();
+					int column = cursor.getColumn();
+					text.setText(row.getText(column));
+					text.selectAll();
+					text.setFocus();
+					text.addKeyListener(new KeyAdapter() {
+						public void keyPressed(KeyEvent e) {
+							// close the text editor and copy the data over
+							// when the user hits "ENTER"
+							if (e.character == SWT.CR) {	//works for both enter keys
+								TreeItem row = cursor.getRow();
+								int column = cursor.getColumn();
+								TreeItemMap.get(row)[column].replace(0, TreeItemMap.get(row)[column].length(), text.getText());
+
+								text.dispose();
+								//refresh(tree);
+								//TODO: increment row
+							}
+							// close the text editor when the user hits "ESC"
+							if (e.character == SWT.ESC) {
+								text.dispose();
+							}
+							if (e.character == SWT.TAB) {
+								//TODO: increment column
+							}
+						}
+					});
+					editor.setEditor(text);
+					text.setFocus();
+
+
+					text.addFocusListener(new FocusAdapter() {
+						public void focusLost(FocusEvent event) {
+							TreeItem row = cursor.getRow();
+							int column = cursor.getColumn();
+
+							TreeItemMap.get(row)[column].replace(0, TreeItemMap.get(row)[column].length(), text.getText());
+
+							text.dispose();
+						}
+					});
+
+				}
+			}
+		});
+
 	}
 
 	//Messes up autoupdating, but at least it *should* work.
@@ -224,7 +306,7 @@ public class DisplayEnterMarks_PopulateStudentView {
 						assessment.setExpanded(true);
 						assessment.setBackground(marksTree.getDisplay().getSystemColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
 					}
-					
+
 				}
 				unit.setExpanded(true);
 				unit.setBackground(marksTree.getDisplay().getSystemColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
